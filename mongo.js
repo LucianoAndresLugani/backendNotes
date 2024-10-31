9df1bf8df75be2483b4979ee048e339cf6ab7255
luganisofia <luganisofia@gmail.com>
2024-10-31 14:37:32 -0300

    asdasd

--- mongo.js
+++ mongo.js
@@ -1,15 +1,30 @@
-const mongoose = require('mongoose');
-const uri = "mongodb+srv://<luciano>:<1816>@cluster0.4mjsf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-async function run() {
-  try {
-    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
-    await mongoose.connection.db.admin().command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.disconnect();
-  }
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
-run().catch(console.dir);
+
+const password = 1816
+const url =
+  `mongodb+srv://luciano:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
